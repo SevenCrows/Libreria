e08f2bd494306c9b85755a029d7e85e0c5c66c70
Diego Leon Torres <leonect27@gmail.com>
2022-03-01 19:17:59 -0500

    Se crea proyectos que conforman el sistema a desarrollar en NET 5

---
+++ 3.Datos/Libreria.Datos/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Libreria.Datos
+{
+    public class Class1
+    {
+    }
+}
---
+++ 5.Utilitario/Libreria.Utilitario/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Libreria.Utilitario
+{
+    public class Class1
+    {
+    }
+}
