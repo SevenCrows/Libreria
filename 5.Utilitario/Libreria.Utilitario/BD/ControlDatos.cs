6f05defc220b541821335551a89c1be4415762b5
Diego Leon Torres <leonect27@gmail.com>
2022-03-01 20:31:39 -0500

    Modificacion funcionalidad para controlar las transacciones en cada capa del proyecto

---
+++ 5.Utilitario/Libreria.Utilitario/BD/ControlDatos.cs
@@ -0,0 +1,45 @@
+﻿namespace Libreria.Utilitario.BD
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using Libreria.Utilitario.Recursos;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ControlDatos<TDBContexto> where TDBContexto : DbContext, new()
+    {
+        public Func<TDBContexto> FuncContexto = new Func<TDBContexto>(() => { return new TDBContexto(); });
+
+        public TDBContexto ContextoBD;
+
+        public ControlDatos()
+        {
+            ContextoBD = FuncContexto.Invoke();
+        }
+               
+
+        public async Task<T> EjecutarTransaccionDAL<T, C>(Func<Task<T>> cuerpoEjecutar) where C : class
+        {
+            T retorno = default(T);
+            try
+            {
+                using (ContextoBD = FuncContexto.Invoke())
+                {
+                    retorno = await cuerpoEjecutar();
+                }
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw new Exception(rcsUtilitario.MsgErrorConcurrencia);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return retorno;
+        }
+
+    }
+}
